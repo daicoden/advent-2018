a15706becf2c04cf2543bc507847c4f90f7a94d3
Matt Wilson <wilson@cfht.hawaii.edu>
2018-12-04 23:40:52 -1000

    Actually time efficient.

--- day5/star9.rb
+++ day5/star9.rb
@@ -6,28 +6,27 @@ def process(chars)
   new_chars = []
   skip = false
   last_char = ''
-  index = nil
-  chars.each_with_index do |char, i|
+  index = 0
+  while index < chars.size
+    char = chars[index]
     if last_char != char && last_char.downcase == char.downcase
-      index = i + 1
+      new_chars.pop
+      last_char = new_chars[-1] || ''
       skip = true
-      break
     else
-      new_chars << last_char unless last_char.empty?
+      new_chars << char
       last_char = char
     end
+    index = index + 1
   end
 
   new_chars << last_char unless last_char.empty? || skip
 
-  if index.nil?
-    new_chars
-  else
-    new_chars + chars[index..-1]
-  end
+   new_chars
 end
 
 def reduce(alchemical, char_skip)
+=begin
   did_work = true
   puts "starting #{char_skip}"
   while did_work
@@ -35,18 +34,22 @@ def reduce(alchemical, char_skip)
 
     did_work = new_alchemical.size != alchemical.size
     alchemical = new_alchemical
+    puts alchemical.size
   end
 
   puts "done with #{char_skip}"
   alchemical
+=end
+  process(alchemical)
 end
 
 
 threads = []
 result = {}
-'abcdefghijklmnopqrstuvwxyz+'.each_char do |skip|
+'+abcdefghijklmnopqrstuvwxyz'.each_char do |skip|
   threads << Thread.new do
-    result[skip] = reduce(alchemical.tr(skip.downcase, '').tr(skip.upcase, '').split(''), skip).size
+    r = reduce(alchemical.tr(skip.downcase, '').tr(skip.upcase, '').split(''), skip)
+    result[skip] = r.size
   end
 end
 
