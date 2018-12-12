e11bd9ff86c2d7015085b0a1121f192a54f5c970
Matt Wilson <wilson@cfht.hawaii.edu>
2018-12-11 16:45:34 -1000

    Damn

--- day11/star21.rb
+++ day11/star21.rb
@@ -1,8 +1,5 @@
-
-
-#$serial_number = 5177
-
-$serial_number = 18
+$serial_number = 5177
+#$serial_number =  42
 
 
 def cell_power(x, y, serial_number)
@@ -13,9 +10,9 @@ def cell_power(x, y, serial_number)
 end
 
 
-puts cell_power(122,79, 57)
-puts cell_power(217,196, 39)
-puts cell_power(101,153, 71)
+puts cell_power(122, 79, 57)
+puts cell_power(217, 196, 39)
+puts cell_power(101, 153, 71)
 
 
 grid = []
@@ -28,37 +25,69 @@ grid = []
 end
 
 
-$cluster_power = []
-
-
-def max_power(x, y, grid)
-  (0...3).each do |xoff|
-    (0...3).each do |yoff|
+def max_power(x, y, size, grid)
+  cluster_power = 0
+  (0...size).each do |xoff|
+    (0...size).each do |yoff|
       if x + xoff > 300 || y + yoff > 300
         return nil
       end
 
-      $cluster_power[x] ||= []
-      $cluster_power[x][y] ||= 0
-      $cluster_power[x][y] += grid[x + xoff][y +yoff]
+      cluster_power += grid[x + xoff][y +yoff]
     end
   end
+  cluster_power
 end
 
-(1..300).each do |x|
-  (1..300).each do |y|
-    max_power(x, y, grid)
+$cluster_power = []
+
+def populate_max_power(size, grid)
+  if size == 1
+    cluster_power = []
+    (1..(300)).each do |x|
+      (1..(300)).each do |y|
+        cluster_power[x] ||= []
+        cluster_power[x][y] = max_power(x, y, size, grid)
+      end
+    end
+    cluster_power
+  else
+    cluster_power = []
+    (1..(300 - size+1)).each do |x|
+      (1..(300 - size+1)).each do |y|
+        cluster_power[x] ||= []
+        cluster_power[x][y] = $cluster_power[size - 1][x][y]
+        (0...(size-1)).each do |extra|
+        #  puts "Looking at #{x}, #{y} size #{size}, adding in #{x+size-1}, #{y + extra}"
+        #  puts "Looking at #{x}, #{y} size #{size}, adding in #{x+extra}, #{y + size - 1}"
+          cluster_power[x][y] += grid[x + size - 1][y + extra]
+          cluster_power[x][y] += grid[x + extra][y + size - 1]
+        end
+       # puts "Bah"
+       # puts "Looking at #{x}, #{y} size #{size}, adding in #{x+size - 1}, #{y + size - 1}"
+        cluster_power[x][y] += grid[x + size - 1][y + size - 1]
+
+        # puts "---"
+      end
+    end
+    cluster_power
   end
 end
 
-max_found = [-100, 0, 0]
+max_found = [-100, 0, 0, 0]
+(1..300).each do |size|
+  puts "investigating #{size} #{max_found}"
+  $cluster_power[size] = populate_max_power(size, grid)
 
-(1..297).each do |x|
-  (1..297).each do |y|
-    if $cluster_power[x][y] > max_found[0]
-      max_found = [$cluster_power[x][y], x, y]
+  (1..(300 - size+1)).each do |x|
+    (1..(300 - size+1)).each do |y|
+      if ($cluster_power[size][x][y] || -100) > max_found[0]
+        max_found = [$cluster_power[size][x][y], x, y, size]
+      end
     end
   end
+
 end
 
-print max_found
+
+puts max_found.inspect
